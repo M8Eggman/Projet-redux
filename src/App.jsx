dc769fccb03edbe7f5d16b88e46e08e1fe7e95c2
GoNEAR <cagatay.morgul@outlook.com>
2025-06-18 13:54:02 +0200

    route fini

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css'
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 export default function App() {
   return (
     <>
-    
+      <Routes>
+        <Route path="/" element>
+          <Route index element/>
+          <Route path="/:custom" element/>
+          <Route path="/validation" element/>
+          <Route path="/remerciement" element/>
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
