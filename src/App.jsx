32ab993682d79c972d23807e783584e5b77ca6e6
GoNEAR <cagatay.morgul@outlook.com>
2025-06-18 16:03:16 +0200

    fix merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import "./App.css"
 import { Routes, Route } from "react-router-dom";
 import AccueilPizza from "./pages/AccueilPizza/AccueilPizza";
+import Layout from "./layout/Layout";
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/">
+        <Route path="/" element={<Layout/>}>
           <Route index element={<AccueilPizza />} />
           {/* <Route path="/:custom" element />
           <Route path="/validation" element />
