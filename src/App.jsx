a7f2dffe07b613a0771a8b86adb3102ff1f1e9ba
GoNEAR <cagatay.morgul@outlook.com>
2025-06-19 10:24:42 +0200

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import "./App.css";
+import "./App.css"
 import { Routes, Route } from "react-router-dom";
 import AccueilPizza from "./pages/AccueilPizza/AccueilPizza";
 import Layout from "./layout/Layout";
-import PizzaPanier from "./components/pizzaPanier/pizzaPanier";
 
 export default function App() {
   return (
     <>
-      <PizzaPanier />
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<AccueilPizza />} />
+          {/* <Route path="/:custom" element />
+          <Route path="/validation" element />
+          <Route path="/remerciement" element /> */}
+        </Route>
+      </Routes>
     </>
   );
 }
