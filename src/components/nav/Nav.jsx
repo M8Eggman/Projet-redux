e42427774a0f81ae772c5757f3f85136be303986
rayan <desmetrayan621@gmail.com>
2025-06-19 09:27:26 +0200

    nav modifier

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./Nav.css"; // Le style à côté
-import logo from "../../assets/logo.webp"
+import "./Nav.css";
+import logo from "../../assets/logo.webp";
 
 export default function Nav() {
   return (
@@ -10,9 +10,25 @@ export default function Nav() {
           <img src={logo} alt="Logo PizzaHut" className="logo-img" />
         </a>
       </div>
+
       <ul className="nav-links">
+        <li className="NOclk">Menu</li>
+        <li className="NOclk">Entrée</li>
         <li><a href="#pizza">Pizza</a></li>
+        <li className="NOclk">Spécialités</li>
+        <li className="NOclk">Boissons</li>
+        <li className="NOclk">Desserts</li>
       </ul>
+
+      <div className="right-section">
+        <ul className="right-links">
+          <li className="se-connecter">Se connecter</li>
+          <li className="langue">
+            <span className="lang-text">FR</span>
+            <span className="chevron">▼</span>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
