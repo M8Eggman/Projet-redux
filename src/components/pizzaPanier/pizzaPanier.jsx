8a24e3905ad6a600f616db1f698102b3f85c78af
GoNEAR <cagatay.morgul@outlook.com>
2025-06-19 10:23:27 +0200

    style panier

--- src/components/pizzaPanier/pizzaPanier.jsx
+++ src/components/pizzaPanier/pizzaPanier.jsx
@@ -1,32 +1,54 @@
 import "./pizzaPanier.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function PizzaPanier() {
   return (
     <div className="pizzaPanier">
-      <h2>Panier d'achat</h2>
-      <div className="pizzaPanierPizza">
-        <div className="pizzaPanierPizzadetails">
-          <p>Garden Lovers</p>
-          <p>Medium Classic</p>
-          <div className="pizzaPanierQuantity">
-            <button>-</button>
-            <span>1</span>
-            <button>+</button>
+      <div className="pizzaPanierHeader">
+        <h2>Panier d'achat</h2>
+        <div className="pizzaPanierAllPizzas">
+          <div className="pizzaPanierPizza">
+            <div className="pizzaPanierPizzadetails">
+              <p>Garden Lovers</p>
+              <p>Medium Classic</p>
+              <div className="pizzaPanierQuantity">
+                <button>
+                  <FontAwesomeIcon icon={faMinus} />
+                </button>
+                <span>1</span>
+                <button>
+                  <FontAwesomeIcon icon={faPlus} />
+                </button>
+              </div>
+            </div>
+            <div className="pizzaPanierPrice">
+              <p>€16,00</p>
+              <div className="pizzaPanierBtn">
+                <button className="pizzaPanierModifier">Modifier</button>
+                <button className="pizzaPanierSupprimer">Supprimer</button>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="pizzaPanierBtn">
-          <p>€16,00</p>
-          <button className="pizzaPanierModifier">Modifier</button>
-          <button className="pizzaPanierSupprimer">Supprimer</button>
+        <input className="pizzaPanierCoupon" type="text" placeholder="Insérer votre coupon" />
+        <div className="pizzaPanierTotal">
+          <p>
+            <span>Livraison</span>
+            <span>€1,99</span>
+          </p>
+          <p>
+            <span>Total </span>
+            <span>€17,99</span>
+          </p>
         </div>
       </div>
-      <input className="pizzaPanierCoupon" type="text" placeholder="Insérer votre coupon" />
-      <div className="pizzaPanierTotal">
-        <span>Livraison €1,99</span>
-        <span>Total €17,99</span>
-      </div>
-      <div className="pizzaPanierBtn">
-        <button>1 Commander</button>
+      <div className="pizzaPanierCommander">
+        <button>
+          <span>1</span>
+          <span>Commander</span>
+          <span>€17,99</span>
+        </button>
       </div>
     </div>
   );
